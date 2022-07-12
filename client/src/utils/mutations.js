224e428e9135b5d6abc8a31fadecfb42fb09e67f
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-12 16:06:21 -0600

    added my mutations to the mutations.js file that i added within the utils directory on the client side to handle creating, updating, and deleting to the server

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook($userId: ID!, $book: String!) {
+    addBook(userId: $userId, book: $book) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      name
+      savedBooks
+    }
+  }
+`;
